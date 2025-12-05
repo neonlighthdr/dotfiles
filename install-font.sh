2594caebdfc78581e4d5a932e632c43670a174df
neonlighthdr <neonlighthdr@gmail.com>
2025-12-05 22:57:32 +0700

    Create install-font.sh

---
+++ install-font.sh
@@ -0,0 +1,4 @@
+# setup font
+curl -L -o ~/.termux/font.ttf https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/Hack/Bold/HackNerdFont-Bold.ttf
+sleep 2
+termux-reload-settings
